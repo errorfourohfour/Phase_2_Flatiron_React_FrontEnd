037231742be693b3b24b1270073b679833887356
errorfourohfour <80006247+errorfourohfour@users.noreply.github.com>
2021-10-24 19:57:41 -0400

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h2>Hello! Here you can make a bucket list. You can go to either activities list, or destination list, and add activities that you would like to do, or places you'd like to visit during your life</h2>
+    </div>
+  );
+}
